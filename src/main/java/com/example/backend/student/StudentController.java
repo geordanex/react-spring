339d2a080ec18f1afb56b4942413f4161bbb8737
geordanex <geordan62@gmail.com>
2022-06-24 00:14:37 -0500

    add jpa with local config and modif UI table

--- src/main/java/com/example/backend/student/StudentController.java
+++ src/main/java/com/example/backend/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.backend.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        List<Student> students = List.of(
-                new Student(1L, "Jamila", "jamila@example.edu", Gender.FEMALE),
-                new Student(2L, "Alex", "alex@example.edu", Gender.MALE)
-        );
-        return students;
+        return studentService.getAllStudents();
     }
 }
